daad1863dbab8cf05c1d88f6099d8e5b301a08af
donisaputradev <donisaputradev@gmail.com>
2024-01-04 09:44:54 +0700

    📄 docs: Membuat Reusable Component dan Props

--- src/views/product/IndexView.vue
+++ src/views/product/IndexView.vue
@@ -2,28 +2,23 @@
   <div>
     <div id="page-wrap">
       <div class="grid-wrap">
-        <div v-for="product in products" :key="product.id" class="product-item">
-          <img :src="product.imageUrl" alt="">
-          <h3>{{ product.name }}</h3>
-          <p class="product-price">Rp{{ product.price }}</p>
-          <router-link :to="{ name: 'product-detail', params: { id: product.id } }">
-            <button>Detail</button>
-          </router-link>
-        </div>
+        <ProductItemComponent v-for="product in products" :key="product.id" :product="product" />
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import ProductItemComponent from "@/components/ProductItemComponent.vue";
 import { products } from "../../data-seed";
 
 export default {
   data() {
     return {
       products
-    }
-  }
+    };
+  },
+  components: { ProductItemComponent }
 }
 </script>
 
@@ -33,35 +28,5 @@ export default {
   flex-wrap: wrap;
   justify-content: space-between;
   margin-top: 16px;
-}
-
-.product-item {
-  align-items: center;
-  border-radius: 8px;
-  box-shadow: 0px 2px 5px #888;
-  display: flex;
-  flex-direction: column;
-  margin-bottom: 2%;
-  padding: 20px;
-  position: relative;
-  width: 32%;
-}
-
-.product-name {
-  margin-bottom: 0;
-}
-
-img {
-  height: 200px;
-  width: 200px;
-  border-radius: 5%;
-}
-
-a {
-  width: 100%;
-}
-
-button {
-  width: 100%;
 }
 </style>
